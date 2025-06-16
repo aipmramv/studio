181a6c7179d4efade3d0f40de39b969d083835aa
Ram V <aipm.ramv@gmail.com>
2025-06-16 11:51:54 +0000

    symbol for INR ₹

--- src/components/features/DcGeneratorContent.tsx
+++ src/components/features/DcGeneratorContent.tsx
@@ -161,7 +161,7 @@ export function DcGeneratorContent() {
                   <TableCell className="font-medium">{item.description}</TableCell>
                   <TableCell className="text-right">{item.quantity}</TableCell>
                   <TableCell>{item.uom}</TableCell>
-                  <TableCell className="text-right">{item.value.toLocaleString('en-IN')}</TableCell>
+                  <TableCell className="text-right">{currencySymbol}{item.value.toLocaleString('en-IN')}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
